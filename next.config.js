4ba489dfe5d297e3d2b0fee5f2ff2f1251252134
Yami <raphaelribeirocaetano@gmail.com>
2025-05-25 22:50:43 -0300

    minor changes: swap next.config.ts to *.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -9,11 +8,11 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/img/**",
       },
-  ]
+    ],
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
